b8618e24c5dba43b3ee6e58abeb051148fdf1772
Dmitriy Derepko <xepozz@list.ru>
2025-09-05 19:21:02 +0400

    feat: annotate calls

--- src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpAnnotator.kt
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpAnnotator.kt
@@ -2,6 +2,7 @@ package com.github.xepozz.php_opcodes_language.language
 
 import com.github.xepozz.php_opcodes_language.Opcodes
 import com.github.xepozz.php_opcodes_language.Primitives
+import com.github.xepozz.php_opcodes_language.language.psi.PHPOpBlockName
 import com.github.xepozz.php_opcodes_language.language.psi.PHPOpLineNumber
 import com.github.xepozz.php_opcodes_language.language.psi.PHPOpParameter
 import com.github.xepozz.php_opcodes_language.language.psi.PHPOpParenParameter
@@ -9,6 +10,7 @@ import com.github.xepozz.php_opcodes_language.language.psi.PHPOpVarName
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.jetbrains.php.lang.highlighter.PhpHighlightingData
 
@@ -43,6 +45,50 @@ class PHPOpAnnotator : Annotator {
                     .textAttributes(attributesKey)
                     .create()
 
+            }
+
+            is PHPOpBlockName -> {
+                when {
+                    element.isFqn -> {
+                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                            .range(element.textRange)
+                            .textAttributes(PhpHighlightingData.FUNCTION)
+                            .create()
+                    }
+
+                    element.isClassMethod -> {
+                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                            .range(
+                                TextRange(
+                                    element.text.indexOf("::") + 2,
+                                    element.text.length
+                                ).shiftRight(element.textRange.startOffset)
+                            )
+                            .textAttributes(PhpHighlightingData.FUNCTION_CALL)
+                            .create()
+                    }
+
+                    element.isClassPropertyHook -> {
+                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                            .range(
+                                TextRange(element.text.indexOf("::") + 2, element.text.lastIndexOf("::")).shiftRight(
+                                    element.textRange.startOffset
+                                )
+                            )
+                            .textAttributes(PhpHighlightingData.INSTANCE_FIELD)
+                            .create()
+
+                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                            .range(
+                                TextRange(
+                                    element.text.lastIndexOf("::") + 2,
+                                    element.text.length
+                                ).shiftRight(element.textRange.startOffset)
+                            )
+                            .textAttributes(PhpHighlightingData.FUNCTION_CALL)
+                            .create()
+                    }
+                }
             }
         }
     }
