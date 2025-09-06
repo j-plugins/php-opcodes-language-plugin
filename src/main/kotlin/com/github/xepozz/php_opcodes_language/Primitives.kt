a03209fe04c7578e2521728e34614067921c0388
Dmitriy Derepko <xepozz@list.ru>
2025-09-06 16:17:28 +0400

    fix: highlight more primitives

--- src/main/kotlin/com/github/xepozz/php_opcodes_language/Primitives.kt
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/Primitives.kt
@@ -2,9 +2,12 @@ package com.github.xepozz.php_opcodes_language
 
 enum class Primitives {
     bool,
+    `true`,
+    `false`,
     int,
     string,
     double,
     `null`,
     array,
+    global,
 }
