39ae8723198bea3efbdb99fee3780b60cde752be
Dmitriy Derepko <xepozz@list.ru>
2025-12-14 12:24:08 +0400

    feat: support comment folding builder

---
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpCommentFoldingBuilder.kt
@@ -0,0 +1,29 @@
+package com.github.xepozz.php_opcodes_language.language
+
+import com.github.xepozz.php_opcodes_language.language.psi.PHPOpBlockComment
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+
+class PHPOpCommentFoldingBuilder : FoldingBuilderEx(), DumbAware {
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean) =
+        collectFoldingBlocks(root).toTypedArray()
+
+    private fun collectFoldingBlocks(element: PsiElement) =
+        PsiTreeUtil
+            .findChildrenOfType(element, PHPOpBlockComment::class.java)
+            .mapNotNull {
+                FoldingDescriptor(
+                    it.node,
+                    it.textRange
+                )
+            }
+
+    override fun getPlaceholderText(node: ASTNode) = node.text.take(15)
+
+    override fun isCollapsedByDefault(node: ASTNode) = true
+}
--- src/main/kotlin/com/github/xepozz/php_opcodes_language/language/psi/PHPOpTokenSets.kt
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/language/psi/PHPOpTokenSets.kt
@@ -5,6 +5,8 @@ import com.intellij.psi.tree.TokenSet
 object PHPOpTokenSets {
     val EMPTY_SET = TokenSet.EMPTY
 
-    val COMMENTS = TokenSet.create(PHPOpTypes.COMMENT)
+    val COMMENTS = TokenSet.EMPTY
+
+    //    val COMMENTS = TokenSet.create(PHPOpTypes.COMMENT)
     val WHITESPACES = TokenSet.WHITE_SPACE
 }
